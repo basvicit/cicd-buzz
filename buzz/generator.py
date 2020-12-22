609f8f3311b987197ebb18a7c3dee5eff5a0de88
basvicit <basvicit@gmail.com>
2020-12-22 10:53:04 +0100

    blank lines toegevoegd en variable naam aangepast

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,16 +1,19 @@
 from __future__ import print_function
 import random
 
+
 buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
-def sample(l, n = 1):
-    result = random.sample(l, n)
+
+def sample(lijst, n = 1):
+    result = random.sample(lijst, n)
     if n == 1:
         return result[0]
     return result
+
 
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
@@ -18,6 +21,6 @@ def generate_buzz():
         sample(verbs), buzz_terms[1]])
     return phrase.title()
 
+
 if __name__ == "__main__":
     print(generate_buzz())
-
