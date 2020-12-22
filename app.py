bbabdc277a8a889a052d505903c61b74165a4b0f
basvicit <76469851+basvicit@users.noreply.github.com>
2020-12-22 12:03:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask
 from buzz import generator
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def generate_buzz():
@@ -10,6 +12,7 @@ def generate_buzz():
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
