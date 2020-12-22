2a32dbbae59ceec990ed4860b2d4f8048628a1ff
basvicit <basvicit@gmail.com>
2020-12-22 10:50:39 +0100

    blank lines toegevoegd aan test_generator

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,9 +1,11 @@
 from buzz import generator
 
+
 def test_sample_single_word():
     lijst = ('foo', 'bar', 'foobar')
     word = generator.sample(lijst)
     assert word in lijst
+
 
 def test_sample_multiple_words():
     lijst = ('foo', 'bar', 'foobar')
@@ -12,6 +14,7 @@ def test_sample_multiple_words():
     assert words[0] in lijst
     assert words[1] in lijst
     assert words[0] is not words[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
